c1080e59317776ae7d46987a6004746a4f60fee4
RAJYA VARDHAN SHUKLA <rajyavardhanshuklakks@gmail.com>
2026-01-01 20:50:39 +0530

    improvement in SMS

--- main.py
+++ main.py
@@ -1,45 +1,64 @@
 import student_ops as so
+
+
 class Student:
-    def __init__(self,roll_no,st_name,st_marks):
+    def __init__(self, roll_no, st_name, st_marks):
         self.roll_num = roll_no
         self.name = st_name
         self.marks = st_marks
 
-print("---Student Management System---")
-print("1. Add student")
-print("2. View all student")
-print("3. Search student by roll number")
-print("4. Update student marks")
-print("5. Delete student")
-print("6. Exit")
 
-selected_option = int(input("Enter your ans: "))
+def main():
+    is_stop = False
 
-if selected_option == 1:
-    roll_num = int(input("Enter student roll number: "))
-    name = input("Enter student name: ")
-    marks = int(input("Enter student marks: "))
-    so.add_Student([roll_num,name,marks])
+    while is_stop != True:
 
-elif selected_option == 2:
-    so.record_view()
+        print("\n---Student Management System---")
+        print("1. Add student")
+        print("2. View all student")
+        print("3. Search student by roll number")
+        print("4. Update student marks")
+        print("5. Delete student")
+        print("6. Exit")
+        selected_option = int(input("Enter your ans: "))
 
-elif selected_option == 3:
-    search_num = input("Enter roll number: ")
-    so.search_student(search_num)
+        if selected_option == 1:
+            stop_adding = False
+            while stop_adding != True:
+                roll_num = int(input("Enter student roll number: "))
+                name = input("Enter student name: ")
+                marks = int(input("Enter student marks: "))
+                so.add_Student([roll_num, name, marks])
+                ans = input("do you want to add more student record? y/n.")
+                if ans == "n":
+                    stop_adding = True
 
-elif selected_option == 4:
-    search_num = input("Enter roll number: ")
-    new_marks = input("Enter new marks: ")
-    so.update_marks(search_num,new_marks)
+        elif selected_option == 2:
+            so.record_view()
 
-elif selected_option == 5:
-    search_num = input("Enter roll number: ")
-    so.delete_student(search_num)    
+        elif selected_option == 3:
+            search_num = input("Enter roll number: ")
+            so.search_student(search_num)
 
-elif selected_option == 6:
-    ans = input("Do you want to stop the existing program? (y/n).").lower()
-    if ans == 'y':
-        input("Existing program is terminated.")
-else:
-    print("Input is invalid. Try again.")
+        elif selected_option == 4:
+            search_num = input("Enter roll number: ")
+            new_marks = input("Enter new marks: ")
+            so.update_marks(search_num, new_marks)
+
+        elif selected_option == 5:
+            search_num = input("Enter roll number: ")
+            so.delete_student(search_num)
+
+        elif selected_option == 6:
+            ans = input("Do you want to stop the existing program? (y/n).").lower()
+            if ans == "y":
+                input("Existing program is terminated.")
+                is_stop = True
+
+                continue
+        elif selected_option > 6 or selected_option < 1:
+            print("Input is invalid. Try again.")
+
+
+if __name__ == "__main__":
+    mai
--- student_ops.py
+++ student_ops.py
@@ -1,59 +1,63 @@
 import csv
 import os
 
+
 def add_Student(new_record):
     try:
-        with open("student.txt","a",newline="") as file:
-         csv.writer(file).writerow(new_record)
-         print("Student record added successfully.")
+        with open("student.txt", "a", newline="") as file:
+            csv.writer(file).writerow(new_record)
+            print("Student record added successfully.")
+            return
     except FileNotFoundError:
         print("File not found.")
+
 
 def record_view():
     try:
-        with open("student.txt","r") as file:
+        with open("student.txt", "r") as file:
             for line in file:
                 print(line.strip())
+        return
     except FileNotFoundError:
         print("File not found.")
 
-        
+
 def search_student(search_no):
     try:
-        with open("student.txt","r") as file:
+        with open("student.txt", "r") as file:
             for line in file:
                 record_list = line.strip().split(",")
                 if record_list[0] == str(search_no):
-                    print("Name: ",record_list[1])
-                    print("Marks: ",record_list[2])
+                    print("Name:", record_list[1])
+                    print("Marks:", record_list[2])
                     return
-            print("Student record not found.") 
-            return   
+            print("Student record not found.")
+            return
     except FileNotFoundError:
         print("File not found.")
 
 
-def update_marks(search_no,new_marks):
+def update_marks(search_no, new_marks):
     try:
         is_updated = False
-        with open("student.txt","r",newline="") as infile:       
-         with open("temp.txt","w",newline="") as outfile:
-            
-            reader = csv.reader(infile)
-            writer = csv.writer(outfile)
+        with open("student.txt", "r", newline="") as infile:
+            with open("temp.txt", "w", newline="") as outfile:
 
-            for row in reader:
-                if row and row[0] == search_no:
-                    row[2] = new_marks
-                    is_updated = True
-                writer.writerow(row)
+                reader = csv.reader(infile)
+                writer = csv.writer(outfile)
+
+                for row in reader:
+                    if row and row[0] == search_no:
+                        row[2] = new_marks
+                        is_updated = True
+                    writer.writerow(row)
         if is_updated:
-                os.replace("temp.txt","student.txt")
-                print("Record is updated.")
+            os.replace("temp.txt", "student.txt")
+            print("Record is updated.")
         else:
-                os.remove("temp.txt")
-                print("Record not found.")
-
+            os.remove("temp.txt")
+            print("Record not found.")
+        return
     except FileNotFoundError:
         print("File not found.")
 
@@ -61,23 +65,23 @@ def update_marks(search_no,new_marks):
 def delete_student(search_no):
     try:
         is_deleted = False
-        with open("student.txt","r",newline="") as infile:       
-         with open("temp.txt","w",newline="") as outfile:
-            
-            reader = csv.reader(infile)
-            writer = csv.writer(outfile)
+        with open("student.txt", "r", newline="") as infile:
+            with open("temp.txt", "w", newline="") as outfile:
 
-            for row in reader:
-                if row and row[0] == search_no:
-                    is_deleted = True
-                    continue
-                writer.writerow(row)
+                reader = csv.reader(infile)
+                writer = csv.writer(outfile)
+
+                for row in reader:
+                    if row and row[0] == search_no:
+                        is_deleted = True
+                        continue
+                    writer.writerow(row)
         if is_deleted:
-                os.replace("temp.txt","student.txt")
-                print("Student record is deleted.")
+            os.replace("temp.txt", "student.txt")
+            print("Student record is deleted.")
         else:
-                os.remove("temp.txt")
-                print("Record not found.")
-
+            os.remove("temp.txt")
+            print("Record not found.")
+        return
     except FileNotFoundError:
         print("File not found.")
