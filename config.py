5764ba40f034138958cd76911ac91b0ad744b2d2
RAJYA VARDHAN SHUKLA <rajyavardhanshuklakks@gmail.com>
2026-03-12 20:04:29 +0530

    removing temp file

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
 STUDENT_FILE = "Student.txt"
-TEMP_FILE = "temp.txt"
 MAX_MARKS = 100
 MIN_MARKS = 0
--- student_ops.py
+++ student_ops.py
@@ -1,5 +1,5 @@
 import os
-from config import STUDENT_FILE, TEMP_FILE
+from config import STUDENT_FILE
 try:
     import pandas as pd
 except ImportError:
